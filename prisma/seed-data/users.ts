117ea71e24dbcccc610b09160dfbb195c2e38fc5
Dharmik <dharmikgodhani071@gmail.com>
2026-08-24 17:51:24 +0530

    chore(seed): update user seed data for test user profile

--- prisma/seed-data/users.ts
+++ prisma/seed-data/users.ts
@@ -12,6 +12,17 @@ const users: userType[] = players.map((player, i) => {
   const index = i + 1;
   const paddedIndex = String(index).padStart(2, "0");
 
+  if (player.userId === "user-1") {
+    return {
+      id: player.userId,
+      googleId: "104164120012759890751",
+      email: "laxit.istosyinfotech@gmail.com",
+      name: "la Istosy",
+      avatarUrl:
+        "https://lh3.googleusercontent.com/a/ACg8ocIMRBRsZAfcwiUOVS4e19ThO-Wod-oGZ4rSQmNTItouEWJz0g=s96-c",
+    };
+  }
+
   return {
     id: player.userId,
     googleId: `google_user_${paddedIndex}`,
